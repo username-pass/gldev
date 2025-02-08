c23cf73d937c66b7940b6cf487a36603b3a8a1d9
username-pass <username-pass.github@github.com>
2025-02-07 16:57:04 -0800

    further added stuff for the compiler, I finally figured out what I need to do for the params! We'll see how it goes. I'm going to make a branch and try to rewrite it now I know what I'm doing. If I manage to do everything with only char, str, and Vecs, I might be able to bootstrap it!

--- compiler.rs
+++ compiler.rs
@@ -13,6 +13,7 @@ fn main() -> std::io::Result<()> {
     
     // create macros map
     let mut macros = Vec::new();
+    let mut params = Vec::new();
     let mut bytecode_stack = Vec::new();
     bytecode_stack.push("testing");
 
@@ -31,32 +32,42 @@ fn main() -> std::io::Result<()> {
     // assert_eq!(contents, "Hello, world!\n");
     for c in contents.chars() {
 
-        // print!("{}",c);
+        print!("{}",c);
 
         //ignore whitespace
         if read_type == 0 && (c == ' ' || c == '\t' || c == '\n') {
             continue;
         }
         else if c == '(' {
+            params[depth].last() = String::from("");
+            params[depth].last().push(c);
             depth += 1;
             read_type = 0;
-            print!("depth: {}\n",depth);
+            params[depth] = Vec::new();
+            // print!("depth: {}\n",depth);
+            print!("{} ", depth);
         }
         else if c == ')' {
+            params[depth].push(c);
             depth -= 1;
             read_type = 0;
+            params[depth+1].push(params[depth].last());
+            // print!("undepth: {}, arg '{}'\n", depth, cur_argument);
+            print!("{} ", depth);
             if cur_argument.chars().count() > 0 {
-                print!("evaluating arg '{}'\n", cur_argument);
+                // print!("evaluating arg '{}' at depth\n", cur_argument);
                 cur_argument = String::from("");
             }
             // print!("moving up in depth\n")
         }
         // now evaluate parameters and functions
-        else if read_type == 0 {
+        else if read_type == 0 { // if it's a command
+            params[depth].last().push(c);
             read_type = 1;
             do_bytecode(c, depth, macros.clone(), &mut bytecode_stack)
         }
         else if read_type == 1 {
+            params[depth].last().push(c);
             cur_argument.push(c);
         }
     }
@@ -66,7 +77,8 @@ fn main() -> std::io::Result<()> {
 fn new_macromap() -> std::collections::HashMap<String, String> {
     return HashMap::new();
 }
-fn do_bytecode(c: char, depth: i32, macros: Vec<std::collections::HashMap<String, String>>, bytecode_stack: &mut Vec<&str>) {
-    print!("running command {}\n", c);
+fn do_bytecode(c: char, depth: usize, macros: Vec<std::collections::HashMap<String, String>>, bytecode_stack: &mut Vec<&str>) {
+    // print!("running command {}\n", c);
+    print!("C");
     bytecode_stack.push("test")
 }
