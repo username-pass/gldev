4a36b2bbf549f0c57c7ee48b6cd77abadb72acf1
username-pass <username-pass.github@github.com>
2025-02-19 23:51:43 -0800

    Paren parser finally works! It now has support for multi-char commands and params. It now outputs the same as my hand-parsed whiteboard output! Updated test case test.gl to test multi-car params and commands. Used new algorithm for paren parsing: Delta is calculated by back-iterating through stack until an unclosed paren is found. Then, that index and depth are subtracted. Also, switched around the order for some of the param and command stuff so that it immediately subtracts the depth after it pushes it to the stack. That way, if it's a single char, it adds it correctly, if it's multi-char, it still works. Also added constants to make token types in code easier to read.

--- compiler.rs
+++ compiler.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -11,24 +10,25 @@ fn main() -> std::io::Result<()> {
     load_defs(&mut defs);
     // actual code, load it into memory
     let mut contents = Vec::new();
-    let mut parens = Vec::new();
     // contents are as follows:
-    // (char, depth, )
+    // (char, type, depth, delta)
+    let mut parens = Vec::new();
+    // parens are as follows
+    // (start, end)
+    // index = depth + delta - 1 (to account for zero indexing)
     read_file(&mut contents, &mut parens)?;
-
+    do_bytecode(&mut contents, &mut parens, &mut defs);
+    // debug
+    // output should match hand-written code
     for (i, (c, token_type, depth, delta)) in contents.iter().enumerate() {
         print!(
             "c: {},\ti:{}\tt: {},\tD: {},\td: {}\n",
             c, i, token_type, depth, delta
         );
     }
-
-    // fill_in_parens(&mut contents);
-
-    // for (c, i) in contents.enumerate() {
-    // do_bytecode(c, i, &mut contents, &mut defs, &mut depth);
-    // print!("{}", c);
-    // }
+    for (i, (start, end, marker)) in parens.iter().enumerate() {
+        print!("idx: {}\t({},\t{})\t{}\n", i, start, end, marker);
+    }
     Ok(())
 }
 
@@ -41,122 +41,68 @@ fn load_defs(defs: &mut Vec<String>) {
     defs.push("cnxn".to_string());
 }
 
-/* fn fill_in_parens(
-    parens: &mut Vec<(usize, usize)>
-) {
-    let mut delta = 0;
-    let mut depth = 0;
-    let mut has_started = false;
-    // Look into this:
-    // may need a second vec to store the positions by a value?!?
-    // Stuff is slightly more complicated than I thought. We'll see
-    // though
-
-    let mut i = 0;
-    for (c, token_type, start, end) in contents.iter() {
-        if *c == '(' {
-            contents[depth + delta].1 = i;
-            depth += 1;
-            if has_started {
-                delta += 1;
-            }
-        } else if *c == ')' {
-            depth -= 1;
-            delta += 1;
-        }
-    }
-} */
-
 fn do_bytecode(
-    c: *mut (char, usize, usize),
-    depth: &mut usize,
-    params: &mut Vec<Vec<String>>,
-    cur_statement: &mut Vec<String>,
-    macros: &mut Vec<String>,
+    contents: &mut Vec<(char, usize, usize, usize)>,
+    parens: &mut Vec<(usize, usize, bool)>,
+    defs: &mut Vec<String>,
 ) {
 }
-
-fn do_bytecode_old(
-    c: char,
-    depth: &mut usize,
-    params: &mut Vec<Vec<String>>,
-    cur_statement: &mut Vec<String>,
-    macros: &mut Vec<String>,
-) {
-    print!("{}", c);
-    if c == '(' {
-        print!("\r\t\t\t\t");
-        for param in &mut *params {
-            print!("\t{:?}", param);
-        }
-        print!("\r\t\t\tlen: {}\tdepth: {}\n", params.len(), depth);
-        *depth += 1;
-        params.push(Vec::new());
-    } else if c == ')' {
-        *depth -= 1;
-        // eval_param(params.last());
-        params.pop();
-        print!("\r\t\t\t\t");
-        for param in &mut *params {
-            print!("\t{:?}", param);
-        }
-        print!("\r\t\t\tlen: {}\tdepth: {}\n", params.len(), depth);
-    } else {
-    }
-}
-
-fn eval_param(param: &mut Option<Vec<String>>) {}
-// {
-
-//     // ensuring that the read type is set in the first place
-//     if cur_statement.len() >= *depth && false {
-//         cur_statement.push(String::from(""));
-//         // params.push(Vec::new());
-//     }
-
-//     if c == '(' {
-//         // increase depth due to paren
-//         *depth += 1;
-//         //create new set of params
-//         params.push(Vec::new());
-//         print!("{}",depth);
-//         cur_statement[*depth].push('(');
-//     }
-//     else if c == ')' {
-//         //add param to prev
-//         let tmp = String::from(&cur_statement[*depth]);
-//         params[*depth].push(tmp.clone());
-//         cur_statement[*depth-1] += &tmp;
-//         // print!("| len: {}\tdepth: {} |", params/*[*depth]*/.len(), depth);
-//         print!("{:?}",params);
-//         *depth -= 1;
-//     }
-// }
 
 fn read_file(
     contents: &mut Vec<(char, usize, usize, usize)>,
-    parens: &mut Vec<(usize, usize)>,
+    parens: &mut Vec<(usize, usize, bool)>,
 ) -> std::io::Result<()> {
     // contents: char, type, depth, delta
     // parens: start, end
     print!("reading file!\n");
     let mut tmp = String::new();
     let mut file = File::open(FILENAME)?;
+    // in the bootstrapped code, this will be implemented by just reading all
+    // characters to the array
     file.read_to_string(&mut tmp)?;
+    //Defining token types
+    let SOLO_PARAM = 0;
+    let COMMAND = 1;
+    let DELIMINATOR = 2;
+    let OPEN_PAREN = 3;
+    let CLOSE_PAREN = 4;
+    //
     let mut depth = 0;
     let mut delta = 0;
-    let mut token_type = 3;
-    let mut last_type = 3;
-    // 0 = solo param
-    // 1 = command
-    // 2 = deliminator between chars (whitespace)
-    // 3 = open paren
-    // 4 = close param
-    print!("tmp: {}\n", tmp);
+    let mut token_type = OPEN_PAREN;
+    let mut last_type = OPEN_PAREN;
+    print!("code:\n{}\n", tmp);
     for (i, c) in tmp.chars().enumerate() {
         last_type = token_type;
-        token_type = 0;
+        token_type = SOLO_PARAM;
         // update deltas and stuff for parens
+
+        if last_type == SOLO_PARAM && token_type != SOLO_PARAM {
+            //changing out of param to not param
+            // add to delta
+            print!("exiting param\n");
+            // WATCH OUT, ITER MIGHT BE WRONG, YOU MAY NEED TO DO
+            // A len - i INSTEAD
+            for (j_rev, (start, end, marker)) in parens.iter().rev().enumerate() {
+                let j = parens.len() - j_rev - 1;
+                print!("backiter, j = {}\n", j);
+                print!("j: {}\tdepth: {}\n", j, depth);
+                if *marker {
+                    continue;
+                }
+                // adding 1 to account for zero indexing
+                // - may remove at some point if it makes things cleaner
+                delta = 1 + j - depth;
+                break;
+            }
+            parens[depth + delta - 1].1 = i - 1;
+            parens[depth + delta - 1].2 = true;
+            depth -= 1;
+        } else if last_type == 4 {
+            // now increment depth
+            depth -= 1;
+            delta += 1;
+        }
 
         if c == '(' {
             // increment depth, move down
@@ -169,60 +115,88 @@ fn read_file(
                 (depth + delta)
             );
             // add to parens list
-            parens.push((i, i));
+            parens.push((i, 0, false));
+            delta = parens.len() - depth;
+            assert!(parens.len() == (depth + delta));
         } else if c == ')' {
             // move this to next iteration to keep close paren inside
             // the depth, next depth only applies on next iteration
             /* depth -= 1;
             delta += 1; */
             token_type = 4;
+
+            // WATCH OUT, ITER MIGHT BE WRONG, YOU MAY NEED TO DO
+            // A len - i INSTEAD
+            //find delta with markers
+            for (j_rev, (start, end, marker)) in parens.iter().rev().enumerate() {
+                let j = parens.len() - j_rev - 1;
+                if *marker {
+                    continue;
+                }
+                // adding 1 to account for zero indexing
+                // - may remove at some point if it makes things cleaner
+                delta = 1 + j - depth;
+                print!("j: {}\tdepth: {}\tdelta: {}\n", j, depth, delta);
+                break;
+            }
+
             // update parens param
+            // print!("depth+delta-1 = {:?}\n", parens[depth + delta - 1]);
             parens[depth + delta - 1].1 = i;
-            print!(
+            parens[depth + delta - 1].2 = true;
+            /* print!(
+                "\t\tparen item:{:?}\tchanged:{}\n",
+                parens[depth + delta - 1],
+                parens[depth + delta - 1].1
+            ); */
+            /* print!(
                 "closing at len: i:{}\tlen:{}\tdepth+delta:{}\n",
                 i,
                 parens.len(),
                 (depth + delta)
-            );
+            ); */
         } else if c == ' ' || c == '\t' || c == '\n' {
             // whitespace
-            token_type = 2;
-        } else if last_type == 3 {
+            token_type = DELIMINATOR;
+        } else if last_type == OPEN_PAREN || last_type == COMMAND {
             // command
             // started with open paren
-            token_type = 1;
-        } else if last_type == 2 {
+            token_type = COMMAND;
+        } else if last_type == DELIMINATOR {
             // if last type was a deliminator
             // it must be a solo param
             // if it's going into a param, increase depth
-            print!("entering param\n");
-            parens.push((i, i));
+            // print!("entering param\n");
+            parens.push((i, i, true));
             depth += 1;
-            token_type = 0;
-        } else {
+            delta = parens.len() - depth;
+            token_type = SOLO_PARAM;
+        } else if last_type == SOLO_PARAM {
+            // assuming that it is a mutli-char param
             // base case, defaulting to param
             print!("base case, char: {}\n", c);
-            token_type = 0;
+            depth += 1;
+            print!("depth: {}\tlen: {}\n", depth, parens.len());
+            print!("delta: {}\n", parens.len() - depth);
+
+            delta = parens.len() - depth;
+            // print!("==LAST===  {}", parens.last().expect((i - 1, i - 1, true)));
+            let len = parens.len();
+
+            parens[len - 1].1 = i;
+
+            token_type = SOLO_PARAM;
         }
 
-        if last_type == 0 && token_type != 0 {
-            //changing out of param to not param
-            // add to delta
-            parens[depth + delta - 1].1 = i - 1;
-            print!("exiting param\n");
-            depth -= 1;
-            delta += 1;
-        } else if last_type == 4 {
-            // now increment depth
-            depth -= 1;
-            delta += 1;
-        }
         // check for single tokens
-        print!(
+        /* print!(
             "c: {}\tlast: {}\ttype: {}\tdepth: {}\tdelta: {}\n",
             c, last_type, token_type, depth, delta
-        );
+        ); */
         contents.push((c, token_type, depth, delta));
+        if token_type == SOLO_PARAM && (last_type == DELIMINATOR || last_type == SOLO_PARAM) {
+            depth -= 1;
+        }
     }
     Ok(())
 }
